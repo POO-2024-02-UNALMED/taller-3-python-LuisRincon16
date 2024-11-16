1a31153bcad21a22de093a3bbe6446b111b3c1ec
LuisRincon16 <lurinconj@unal.edu.co>
2024-11-16 18:57:51 +0000

    int2

--- televisores/tv.py
+++ televisores/tv.py
@@ -2,6 +2,8 @@ from marca import Marca
 from control import Control
 
 class TV:
+    numTV = 0
+
     def __init__(self, marca, estado):
         self._marca: Marca = marca
         self._estado: bool = estado
@@ -9,28 +11,43 @@ class TV:
         self._precio: int = 500
         self._volumen: int = 1
         self._control: Control
+        self.tvCreados()
 
-        def setMarca(self, marc: Marca):
-            self._marca = marc
-        def getMarca(self):
-            return self._marca
+    @classmethod
+    def tvCreados(cls):
+        cls.numTV = cls.numTV + 1
 
-        def setCanal(self, can):
-            self._canal = can
-        def getCanal(self):
-            return self._canal
+    def turnOn(self):
+        self.estado = True
+
+    def turnOff(self):
+        self.estado = False
+
+    def getEstado(self):
+        return self.estado
+
+    def setMarca(self, marc: Marca):
+        self._marca = marc
+    def getMarca(self):
+        return self._marca
+
+    def setCanal(self, can):
+        self._canal = can
+    def getCanal(self):
+        return self._canal
         
-        def setPrecio(self, prec):
-            self._precio = prec
-        def getPrecio(self):
-            return self._precio
+    def setPrecio(self, prec):
+        self._precio = prec
+    def getPrecio(self):
+        return self._precio
         
-        def setVolumen(self, vol):
-            self._volumen = vol
-        def getVolumen(self):
-            return self._volumen
+    def setVolumen(self, vol):
+        self._volumen = vol
+    def getVolumen(self):
+        return self._volumen
         
-        def setControl(self, contr: Control):
-            self._control = contr
-        def getControl(self):
-            return self._control
+    def setControl(self, contr: Control):
+        self._control = contr
+    def getControl(self):
+        return self._control
+    
