3c14fc122b2d2381fb2270556047f5ed8263ff57
LuisRincon16 <lurinconj@unal.edu.co>
2024-11-19 00:15:47 +0000

    int13

--- televisores/tv.py
+++ televisores/tv.py
@@ -1,5 +1,5 @@
-from televisores.marca import Marca
-from televisores.control import Control
+#from televisores.marca import Marca
+#from televisores.control import Control
 
 class TV:
     _numTV = 0
